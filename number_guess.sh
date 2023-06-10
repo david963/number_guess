703b49436d3f5efc6abba09ffec21146b4fd6197
david963 <davidguerrero963@yahoo.com>
2023-06-09 22:15:27 -0500

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -2,4 +2,15 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo Enter your username:
-read USERNAME
+read NAME
+USERNAME=$($PSQL "SELECT username FROM games WHERE username = '$NAME'")
+if [[ -z $USERNAME ]]
+then
+  echo Welcome, $NAME! It looks like this is your first time here.
+fi
+echo Guess the secret number between 1 and 1000:
+read NUMBER
+if [[ ! $NUMBER =~ ^[0-9]+$  ]]
+then
+  echo That is not an integer, guess again:
+fi
