11101ddd6f2c104b326c0834ee50fda4b9396e9d
david963 <davidguerrero963@yahoo.com>
2023-06-11 19:52:24 -0500

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -6,20 +6,23 @@ COUNT=1
 echo Enter your username:
 read NAME
 USERNAME=$($PSQL "SELECT username FROM games WHERE username = '$NAME'")
+GAMES_PLAYED=$($PSQL "SELECT games_played FROM games WHERE username = '$NAME'")
+BEST_GAME=$($PSQL "SELECT best_game FROM games WHERE username = '$USERNAME'")
 # if username doesn't exist, insert into record
 if [[ -z $USERNAME ]]
 then
   INSERT_USERNAME=$($PSQL "INSERT INTO games VALUES('$NAME')")
   USERNAME=$($PSQL "SELECT username FROM games WHERE username = '$NAME'")
   echo Welcome, $USERNAME! It looks like this is your first time here.
+else
+  echo Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses.
 fi
 echo Guess the secret number between 1 and 1000:
 read NUMBER 
+echo $RANDOM_NUMBER IS RANDOM NUMBER
 
-
-while [ $NUMBER -ne $RANDOM_NUMBER ]
+while [ true ]
 do
-# echo $RANDOM_NUMBER
   if [[ ! $NUMBER =~ ^[0-9]+$  ]]
   then
     echo That is not an integer, guess again:
@@ -27,13 +30,16 @@ do
   then
     echo It\'s higher than that, guess again:
     ((COUNT++))
-  else
+  elif [[ $NUMBER -gt $RANDOM_NUMBER ]]
+  then
     echo It\'s lower than that, guess again:
     ((COUNT++))
+  else
+    break;
   fi
   read NUMBER
 done
-BEST_GAME=$($PSQL "SELECT best_game FROM games WHERE username = '$USERNAME'")
+
 if [[ $COUNT -lt $BEST_GAME || -z $BEST_GAME ]]
 then
   BEST_GAME=$COUNT
